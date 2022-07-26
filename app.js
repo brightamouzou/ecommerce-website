6df9dbc59023251e3656ea33e417b6a424b9fd9a
brightamouzou <brightamouzou@gmail.com>
2022-07-26 21:36:12 +0000

    production_settings_changes

--- app.js
+++ app.js
@@ -63,20 +63,20 @@ app.use("/api/admin", adminRoutes);
 
 
 //Serve frontend
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      path.resolve(__dirname,"client", "build", "index.html")
     )
   );
-}else{
+// }else{
     
-    // res.send("Please run to production")
-    console.log("Run to production");
-    
-}
+//     // res.send("Please run to production")
+//     console.log("Run to production");
+
+// }
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Running on", port));
 
