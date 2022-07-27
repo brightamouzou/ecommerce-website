2256a570631fd8e3de61ae5d4688ec4a87c744b9
brightamouzou <brightamouzou@gmail.com>
2022-07-27 19:02:05 +0000

    fix bugs14

--- client/src/components/Header/Mobile/index.jsx
+++ client/src/components/Header/Mobile/index.jsx
@@ -2,15 +2,18 @@ import WrapperTemplate from '../../Drawers/Wrapper';
 import HeaderOtherIcons from '../HeaderOtherIcons';
 import MenuOptions from './MenuOptions';
 import Logo from "../../../assets/images/logo.jpg"
+import { Link } from 'react-router-dom';
 
 function MobileHeader() {
   const DrawerHeader=()=>{
     return (
-      <div className="header__mobile__drawerHeader">
-        <div className="brand">
-          <img src={Logo} alt="" />
+      <Link to={"/"}>
+        <div className="header__mobile__drawerHeader">
+          <div className="brand">
+            <img src={Logo} alt="" />
+          </div>
         </div>
-      </div>
+      </Link>
     );
   }
   return (
